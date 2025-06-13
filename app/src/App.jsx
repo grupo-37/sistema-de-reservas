cd9f7fbb78dd18e92f5b217f05cda49294b23dd0
servo pc <fernandonando98@live.com.mx>
2025-06-12 20:59:57 -0600

    feat: Bootstrap and router installed, first layout

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,8 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router";
+
+// Layouts
+import MainLayout from "./layouts/MainLayout";
+
+// Pages
+import Home from "./pages/Home";
+
 function App() {
   return (
-    <>
-      <h1>APP</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
