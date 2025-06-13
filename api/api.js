9bd1bcca2c9ea19d23bb531c9ac2a454f30195a0
servo pc <fernandonando98@live.com.mx>
2025-06-12 19:54:15 -0600

    feat: Mongo models

--- api/api.js
+++ api/api.js
@@ -1,10 +1,29 @@
 import express from "express";
+import Host from "./models/Host.js";
 
 const api = express();
 
 api.get("/", (req, res) => {
   res.json({
     message: "API funcionando",
+  });
+});
+
+api.get("/test", async (req, res) => {
+  const host = await Host.create({
+    firstName: "nombre",
+    lastName: "apellido",
+    email: "test@test.com",
+    password: "123",
+    birthday: new Date(),
+    phone: "1234567890",
+    rfc: "RFC123456",
+    address: "Calle Falsa 123",
+  });
+
+  res.json({
+    message: "Test endpoint funcionando",
+    host,
   });
 });
 
