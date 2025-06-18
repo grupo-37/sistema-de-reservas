25ca1ff33d35d02117ba009dbcd7660bcd17e79a
Ricardo Leon <rickyroses@hotmail.com>
2025-06-17 21:50:31 -0400

    actualizacion con main 2

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -1,18 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-<<<<<<< HEAD
-=======
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
->>>>>>> 602b1c6a0324f91e416966892b8a18ce465a2712
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
   </StrictMode>
-<<<<<<< HEAD
 );
-=======
-);
->>>>>>> 602b1c6a0324f91e416966892b8a18ce465a2712
