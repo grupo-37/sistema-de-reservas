e56cc5310643d1c43150b72f378008b82b59f8f6
servo pc <fernandonando98@live.com.mx>
2025-06-19 21:01:23 -0600

    feat: Register host route and controller with generic body validator middleware

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,8 +1,15 @@
 import express from "express";
 import { registerHost } from "../controllers/auth.controller.js";
+import registerHostValidator from "../validators/registerHost.validator.js";
+import validateBody from "../middlewares/validateBody.js";
 
 const authRoutes = express.Router();
 
-authRoutes.post("/register/host", registerHost);
+// Ruta POST para registrar un nuevo host con validación
+authRoutes.post(
+  "/register/host",
+  validateBody(registerHostValidator),
+  registerHost
+);
 
 export default authRoutes;
