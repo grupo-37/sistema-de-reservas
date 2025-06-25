aa5f3c1f9f3d4889a426398f24443f52c37d9d8b
servo pc <fernandonando98@live.com.mx>
2025-06-24 19:19:01 -0600

    fix: bcrypt to brcryptjs

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,41 +1,41 @@
-import Host from '../models/Host.js';
-import User from '../models/User.js';
-import bcrypt from 'bcrypt';
+import Host from "../models/Host.js";
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
 
-export const registerHost = async (req,res)=> {
-    try {        
-        const { password, ...rest} = req.body;
-        const hashedPassword = await bcrypt.hash(password, 10)
+export const registerHost = async (req, res) => {
+  try {
+    const { password, ...rest } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-        const userExist = await User.findOne({email: rest.email});
+    const userExist = await User.findOne({ email: rest.email });
 
-        if( userExist) {
-            return res.status(400).json({ message: 'User already exists' });
-        }
-
-        const host = await Host.create({
-            ...rest,
-            password: hashedPassword,
-            role: 'Host'
-        });
-
-        if(host){
-            res.status(201).json({
-                _id: host._id,
-                firstName: host.firstName,
-                lastName: host.lastName,
-                email: host.email,
-                birthday: host.birthday,
-                phone: host.phone,
-                address: host.address,
-                rfc: host.rfc,
-                role: host.role,
-            });
-        } else {
-            res.status(400).json({ message: 'Invalid host data' });
-        }
-    } catch (error) {
-        res.status(500).json({ message: 'Server Error: Error creating host' });
-        console.error('Error creating host:', error);
+    if (userExist) {
+      return res.status(400).json({ message: "User already exists" });
     }
-}
+
+    const host = await Host.create({
+      ...rest,
+      password: hashedPassword,
+      role: "Host",
+    });
+
+    if (host) {
+      res.status(201).json({
+        _id: host._id,
+        firstName: host.firstName,
+        lastName: host.lastName,
+        email: host.email,
+        birthday: host.birthday,
+        phone: host.phone,
+        address: host.address,
+        rfc: host.rfc,
+        role: host.role,
+      });
+    } else {
+      res.status(400).json({ message: "Invalid host data" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Server Error: Error creating host" });
+    console.error("Error creating host:", error);
+  }
+};
