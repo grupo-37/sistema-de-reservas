8e20a93aa1a41c59eba3ab8075ffbc5b16db8165
Ricardo Leon <rickyroses@hotmail.com>
2025-06-24 20:07:59 -0400

    feat: actuliazacion codigo

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,25 +1,38 @@
-import User from '../models/User.js';
-import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken'; 
-import { keyToken } from '../config/constants.js';
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { keyToken } from "../config/constants.js";
 
-const generateToken = (userId) => {
-    return jwt.sign({id}, keyToken, { expiresIn: '30d' });
+const generateToken = (id) => {
+    return jwt.sign({ id }, keyToken, { expiresIn: "30d" });
 };
 
 export const authenticateUser = async (req, res) => {
-    const {email, password} = req.body;
-    const userId = await User.findOne({email})
-    
-    if (userId && (await bcrypt.compare(password, userId.password))) {
-        res.json({
-            _id: userId._id,
-            name: userId.name,
-            tokenAccess: generateToken(userId._id),
-        });
-        res.status(200).json({message:'Token', token: generateToken(userId._id)});
-    } else {
-        res.status(401).json({message: 'Credenciales inválidas'});
+    try {
+    const { email, password } = req.body;
+
+    // 1. Encontrar el usuario por email
+    const user = await User.findOne({ email });
+    if (!user) {
+    return res.status(401).json({ message: "Credenciales inválidas" });
     }
 
-}
+    // 2. Verificar la contraseña
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+    return res.status(401).json({ message: "Credenciales inválidas" });
+    }
+
+    // 3. Generar el token si todo es correcto
+    const token = generateToken(user._id);
+    return res.status(200).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        token,
+    });
+    } catch (err) {
+    console.error(err);
+    return res.status(500).json({ message: "Error interno del servidor" });
+    }
+};
