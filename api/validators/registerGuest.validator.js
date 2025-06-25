89f6bdb1b73d6f32d15bbc2f97f2c7e80d9f96df
leo ocampo <ocampoleonardo409@gmail.com>
2025-06-24 21:30:51 -0600

    se arreglaron los  conflictos

---
+++ api/validators/registerGuest.validator.js
@@ -0,0 +1,32 @@
+import { body } from "express-validator";
+
+const validator = [
+    body("firstName")
+        .isString()
+        .notEmpty()
+        .withMessage("El nombre es obligatorio"),
+    body("lastName")
+        .isString()
+        .notEmpty()
+        .withMessage("El apellido es obligatorio"),
+    body("email").isEmail().withMessage("Correo electrónico inválido"),
+    body("password")
+        .isString()
+        .isLength({ min: 10 })
+        .withMessage("La contraseña debe tener al menos 10 caracteres"),
+    body("birthday")
+        .optional({ checkFalsy: true })
+        .matches(/^\d{4}-\d{2}-\d{2}$/)
+        .withMessage("La fecha de nacimiento debe tener formato válido (YYYY-MM-DD)" ),
+    body("phone")
+        .optional()
+        .isMobilePhone("any")
+        .withMessage("Número de teléfono inválido"),
+    body("paymentMethod")
+        .optional()
+        .isString()
+        .withMessage("Método de pago inválido"),
+    validate,
+];
+
+export default validator;
