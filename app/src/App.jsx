1137f1b1d325bee79ac416cc1a8fff5711486684
servo pc <fernandonando98@live.com.mx>
2025-07-03 20:45:40 -0600

    feat: chat

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -7,20 +7,24 @@ import MainLayout from "./layouts/MainLayout";
 import Home from "./pages/Home";
 import NewPropertyPage from "./pages/NewPropertyPage";
 import RegisterHost from "./pages/RegisterHost";
+import Chat from "./pages/Chat";
 
 function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route element={<MainLayout />}>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/dashboard/host/properties/new" element={<NewPropertyPage />} />
-                    <Route path="/register/host" element={<RegisterHost />} />
-                </Route>
-            </Routes>
-        </BrowserRouter>
-    );
-  
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/dashboard/host/properties/new"
+            element={<NewPropertyPage />}
+          />
+          <Route path="/register/host" element={<RegisterHost />} />
+          <Route path="/chat" element={<Chat />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
