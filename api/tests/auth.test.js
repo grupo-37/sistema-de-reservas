fbcac5a91040887a4cba74c9b6702262bc451928
servo pc <fernandonando98@live.com.mx>
2025-07-24 20:57:03 -0600

    feat: update test environment and add new auth tests

--- api/tests/auth.test.js
+++ api/tests/auth.test.js
@@ -1,7 +1,21 @@
 import request from "./supertest";
+import mongoose from "mongoose";
+
+beforeEach(async () => {
+  const collections = mongoose.connection.collections;
+
+  for (const key in collections) {
+    const collection = collections[key];
+    await collection.deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  await mongoose.connection.close();
+});
 
 describe("Auth API Tests", () => {
-  test("/register host", async () => {
+  test("/register host Registrar nuevo host", async () => {
     const response = await request.post("/api/auth/register/host").send({
       firstName: "John",
       lastName: "Doe",
@@ -12,7 +26,35 @@ describe("Auth API Tests", () => {
       address: "123 Main St",
       rfc: "ABC123456789",
     });
-    console.log(response.body);
     expect(response.statusCode).toBe(201);
+  });
+
+  test("/register host Correo duplicado", async () => {
+    // Registrar a un host
+    const response = await request.post("/api/auth/register/host").send({
+      firstName: "John",
+      lastName: "Doe",
+      email: "john.doe@example.com",
+      password: "password123",
+      birthday: "1990-01-01",
+      phone: "1234567890",
+      address: "123 Main St",
+      rfc: "ABC123456789",
+    });
+    // ver que regresa un 201
+    expect(response.statusCode).toBe(201);
+    // Registrar a otro host con el mismo correo
+    const response2 = await request.post("/api/auth/register/host").send({
+      firstName: "John",
+      lastName: "Doe",
+      email: "john.doe@example.com",
+      password: "password123",
+      birthday: "1990-01-01",
+      phone: "1234567890",
+      address: "123 Main St",
+      rfc: "ABC123456789",
+    });
+    // ver que regresa un 400
+    expect(response2.statusCode).toBe(400);
   });
 });
--- api/tests/supertest.js
+++ api/tests/supertest.js
@@ -1,5 +1,6 @@
 import supertest from "supertest";
 import api from "../api.js";
+import "../config/database.js";
 
 const request = supertest(api);
 
