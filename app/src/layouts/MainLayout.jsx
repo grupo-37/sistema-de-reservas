486025cb3e9f479b9da483a0fafc1f8ef0bdf2f1
servo pc <fernandonando98@live.com.mx>
2025-07-31 21:26:49 -0600

    feat: navbar links

--- app/src/layouts/MainLayout.jsx
+++ app/src/layouts/MainLayout.jsx
@@ -1,9 +1,19 @@
-import { Outlet } from "react-router";
+import { Outlet, Link } from "react-router";
 
 const MainLayout = () => {
   return (
     <div>
       <h1>navbar</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/register/host">Register Host</Link>
+          </li>
+        </ul>
+      </nav>
 
       <div className="container-xl">
         <Outlet />
